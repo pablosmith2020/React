58aeca2d0f2d7d31e8c73d60402c3a8fc6235a0e
Pablo Smith <pablomsmith@hotmail.com>
2021-04-23 21:42:46 -0300

    Desafio 11 Finalizado

--- entrega-final/src/components/body/Cart/Cart.js
+++ entrega-final/src/components/body/Cart/Cart.js
@@ -3,6 +3,7 @@ import Select from "@material-ui/core/Select";
 import React, { useContext } from "react";
 import { CartContext } from "../../CartContext/CartContext";
 import { Link } from "react-router-dom";
+import CartEmpty from "../../../img/CartEmpty.png";
 
 import foto1 from "../../../img/cart-dropdown/00.jpg";
 
@@ -23,144 +24,185 @@ const Card = () => {
     });
   };
 
+  const StyleCartEmpty = {
+    color: "blue",
+    textAlign: "center",
+  };
+
   return (
-    <div id="shoppingCart" className="table-responsive shopping-cart">
-      <table id="root" className="table">
-        <thead id="thead1">
-          <tr id="tr1">
-            <th id="th1">Producto</th>
-            <th className="text-center" id="th-header1">
-              Cantidad
-            </th>
-            <th className="text-center" id="th-header2">
-              Sub Total
-            </th>
-            <th className="text-center" id="th-header3">
-              Descuento
-            </th>
-            <th className="text-center" id="th-header4">
-              <a
-                className="btn btn-sm btn-outline-danger"
-                href="#"
-                onClick={() => DeleteAllProdcuct()}
-              >
-                Eliminar Productos
-              </a>
-            </th>
-          </tr>
-        </thead>
-        <tbody id="tbody">
-          {Cart?.map((Car) => {
-            //console.log(Car.colour)
-            return (
-              <tr id={`tr-${Car.id}`} key={`tr-${Car.id}`}>
-                <td id={`td-${Car.id}`}>
-                  <div id={`divProduct-${Car.id}`} className="product-item">
-                    <a id={`a-${Car.id}`} className="product-thumb" href="/#">
-                      <img id={`img-${Car.id}`} src={foto1} alt="Imagen"></img>
-                    </a>
-                    <div id={`divProduct2-${Car.id}`} className="product-info">
-                      <h4 id={`h4-${Car.id}`} className="product-title">
-                        <a id={`a2-${Car.id}`} href="/#">
-                          {Car.description}
-                        </a>
-                      </h4>
-                      <span id={`span1-${Car.id}`}>
-                        <em id={`em1-${Car.id}`}>{`Tamaño: ${Car.size}`} </em>
-                      </span>
-                      <span id={`span2-${Car.id}`}>
-                        <em id={`em-${Car.id}`}>{`Color: ${Car.colour}`}</em>
-                      </span>
-                    </div>
-                  </div>
-                </td>
-                <td id={`td2-${Car.id}`} className="text-center">
-                  <div id={`div2-${Car.id}`} className="count-input">
-                    <Select
-                      className="form-control"
-                      native
-                      value={Car.count}
-                      onChange={handleChange}
-                      inputProps={{
-                        name: "count",
-                        id: "countProduct",
-                      }}
-                    >
-                      <option aria-label="None" value="" />
-                      <option value={1}>1</option>
-                      <option value={2}>2</option>
-                      <option value={3}>3</option>
-                      <option value={4}>4</option>
-                      <option value={5}>5</option>
-                    </Select>
-                  </div>
-                </td>
-                <td
-                  id={`td3-${Car.id}`}
-                  className="text-center text-lg text-medium"
-                >
-                  {" "}
-                  $ {Car.price}
-                </td>
-                <td
-                  id={`td4-${Car.id}`}
-                  className="text-center text-lg text-medium"
-                >
-                  {Car.discount} %
-                </td>
-                <td id={`td5-${Car.id}`} className="text-center">
-                  <a id={`a3-${Car.id}`} className="remove-from-cart" href="#">
-                    <i
-                      id={`i2-${Car.id}`}
-                      className="icon-cross"
-                      onClick={() => DeleteProdcuct(Car.id)}
-                    >
-                      X
-                    </i>
+    <div>
+      {Cart.length === 0 ? (
+        <div>
+          <section style={StyleCartEmpty}>
+            <h1>No tienes ningún artículo en tu carrito de compras.</h1>
+            <img src={CartEmpty} alt="Carrito Vacio"></img>
+          </section>
+        </div>
+      ) : (
+        <div id="shoppingCart" className="table-responsive shopping-cart">
+          <table id="root" className="table">
+            <thead id="thead1">
+              <tr id="tr1">
+                <th id="th1">Producto</th>
+                <th className="text-center" id="th-header1">
+                  Cantidad
+                </th>
+                <th className="text-center" id="th-header2">
+                  Sub Total
+                </th>
+                <th className="text-center" id="th-header3">
+                  Descuento
+                </th>
+                <th className="text-center" id="th-header4">
+                  <a
+                    className="btn btn-sm btn-outline-danger"
+                    href="#"
+                    onClick={() => DeleteAllProdcuct()}
+                  >
+                    Eliminar Productos
                   </a>
-                </td>
+                </th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody id="tbody">
+              {Cart?.map((Car) => {
+                //console.log(Car.colour)
+                return (
+                  <tr id={`tr-${Car.id}`} key={`tr-${Car.id}`}>
+                    <td id={`td-${Car.id}`}>
+                      <div id={`divProduct-${Car.id}`} className="product-item">
+                        <a
+                          id={`a-${Car.id}`}
+                          className="product-thumb"
+                          href="/#"
+                        >
+                          <img
+                            id={`img-${Car.id}`}
+                            src={foto1}
+                            alt="Imagen"
+                          ></img>
+                        </a>
+                        <div
+                          id={`divProduct2-${Car.id}`}
+                          className="product-info"
+                        >
+                          <h4 id={`h4-${Car.id}`} className="product-title">
+                            <a id={`a2-${Car.id}`} href="/#">
+                              {Car.description}
+                            </a>
+                          </h4>
+                          <span id={`span1-${Car.id}`}>
+                            <em id={`em1-${Car.id}`}>
+                              {`Tamaño: ${Car.size}`}{" "}
+                            </em>
+                          </span>
+                          <span id={`span2-${Car.id}`}>
+                            <em
+                              id={`em-${Car.id}`}
+                            >{`Color: ${Car.colour}`}</em>
+                          </span>
+                        </div>
+                      </div>
+                    </td>
+                    <td id={`td2-${Car.id}`} className="text-center">
+                      <div id={`div2-${Car.id}`} className="count-input">
+                        <Select
+                          className="form-control"
+                          native
+                          value={Car.count}
+                          onChange={handleChange}
+                          inputProps={{
+                            name: "count",
+                            id: "countProduct",
+                          }}
+                        >
+                          <option aria-label="None" value="" />
+                          <option value={1}>1</option>
+                          <option value={2}>2</option>
+                          <option value={3}>3</option>
+                          <option value={4}>4</option>
+                          <option value={5}>5</option>
+                        </Select>
+                      </div>
+                    </td>
+                    <td
+                      id={`td3-${Car.id}`}
+                      className="text-center text-lg text-medium"
+                    >
+                      {" "}
+                      $ {Car.price}
+                    </td>
+                    <td
+                      id={`td4-${Car.id}`}
+                      className="text-center text-lg text-medium"
+                    >
+                      {Car.discount} %
+                    </td>
+                    <td id={`td5-${Car.id}`} className="text-center">
+                      <a
+                        id={`a3-${Car.id}`}
+                        className="remove-from-cart"
+                        href="#"
+                      >
+                        <i
+                          id={`i2-${Car.id}`}
+                          className="icon-cross"
+                          onClick={() => DeleteProdcuct(Car.id)}
+                        >
+                          X
+                        </i>
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
 
-      <div className="shopping-cart-footer">
-        <div className="column">
-          <form className="coupon-form" method="post">
-            <input
-              className="form-control form-control-sm"
-              type="text"
-              placeholder="Numero de Cupon"
-              required=""
-            ></input>
-            <button className="btn btn-outline-primary btn-sm" type="submit">
-              Aplicar Cupon
-            </button>
-          </form>
-        </div>
-        <div className="column text-lg">
-          Subtotal:{" "}
-          <span id="spanTotalAmount" className="text-medium">
-            {TotalAmount}
-          </span>
-        </div>
-      </div>
+          <div className="shopping-cart-footer">
+            <div className="column">
+              <form className="coupon-form" method="post">
+                <input
+                  className="form-control form-control-sm"
+                  type="text"
+                  placeholder="Numero de Cupon"
+                  required=""
+                ></input>
+                <button
+                  className="btn btn-outline-primary btn-sm"
+                  type="submit"
+                >
+                  Aplicar Cupon
+                </button>
+              </form>
+            </div>
+            <div className="column text-lg">
+              Subtotal:{" "}
+              <span id="spanTotalAmount" className="text-medium">
+                {TotalAmount}
+              </span>
+            </div>
+          </div>
 
-      <div className="shopping-cart-footer">
-        <div className="column">
-          <a className="btn btn-outline-secondary shop-grid-ls.html" href="/#">
-            <Link to="/">
-              <i className="icon-arrow-left"></i>&nbsp;Ir a la Tienda
-            </Link>
-          </a>
+          <div className="shopping-cart-footer">
+            <div className="column">
+              <a
+                className="btn btn-outline-secondary shop-grid-ls.html"
+                href="/#"
+              >
+                <Link to="/">
+                  <i className="icon-arrow-left"></i>&nbsp;Ir a la Tienda
+                </Link>
+              </a>
+            </div>
+            <div className="column">
+              <a className="btn btn-success" href="/#">
+                Comprar
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="column">
-          <a className="btn btn-success" href="/#">
-            Comprar
-          </a>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- entrega-final/src/components/header/HeaderCart.js
+++ entrega-final/src/components/header/HeaderCart.js
@@ -19,87 +19,90 @@ const HeaderCart = () => {
       <span id="TotalPurchaseCart" className="subtotal">
         {TotalAmount}
       </span>
-      <div className="toolbar-dropdown" id="ShoppingToolbar-dropdown">
-        {Cart.map((Car) => {
-          //console.log(Category)
-          return (
-            <div
-              id={`divThird-${Car.id}`}
-              className="dropdown-product-item"
-              key={Car.id}
-            >
-              <span
-                id={`spanFirst-${Car.id}`}
-                className="dropdown-product-remove"
+
+      {Cart.length === 0 ? null : (
+        <div className="toolbar-dropdown" id="ShoppingToolbar-dropdown">
+          {Cart.map((Car) => {
+            //console.log(Category)
+            return (
+              <div
+                id={`divThird-${Car.id}`}
+                className="dropdown-product-item"
+                key={Car.id}
               >
-                <i
-                  id={Car.id}
-                  className="icon-cross"
-                  onClick={() => DeleteProdcuct(Car.id)}
+                <span
+                  id={`spanFirst-${Car.id}`}
+                  className="dropdown-product-remove"
                 >
-                  X
-                </i>
-              </span>
-              <a
-                id={`aSecond-${Car.id}`}
-                className="dropdown-product-thumb"
-                href="/#"
-              >
-                <img id={`img-${Car.id}`} src={foto1} alt="Imagen Producto" />
-              </a>
-              <div id={`div-${Car.id}`} className="dropdown-product-info">
+                  <i
+                    id={Car.id}
+                    className="icon-cross"
+                    onClick={() => DeleteProdcuct(Car.id)}
+                  >
+                    X
+                  </i>
+                </span>
                 <a
-                  id={`aFirst-${Car.id}`}
-                  className="dropdown-product-title"
+                  id={`aSecond-${Car.id}`}
+                  className="dropdown-product-thumb"
                   href="/#"
                 >
-                  {Car.description}
+                  <img id={`img-${Car.id}`} src={foto1} alt="Imagen Producto" />
                 </a>
+                <div id={`div-${Car.id}`} className="dropdown-product-info">
+                  <a
+                    id={`aFirst-${Car.id}`}
+                    className="dropdown-product-title"
+                    href="/#"
+                  >
+                    {Car.description}
+                  </a>
+                </div>
+                <span
+                  id={`spanLast-${Car.id}`}
+                  className="dropdown-product-detailse"
+                >
+                  {Car.count} x {Car.price}
+                </span>
               </div>
-              <span
-                id={`spanLast-${Car.id}`}
-                className="dropdown-product-detailse"
-              >
-                {Car.count} x {Car.price}
+            );
+          })}
+          <div id="divFooter" className="toolbar-dropdown-group">
+            <div id="divTotal" className="column">
+              <span id="spanTotal" className="text-lg">
+                Total:
               </span>
             </div>
-          );
-        })}
-        <div id="divFooter" className="toolbar-dropdown-group">
-          <div id="divTotal" className="column">
-            <span id="spanTotal" className="text-lg">
-              Total:
-            </span>
+            <div id="divPrecio" className="column text-right">
+              <span id="spanPrecio" className="text-lg text-medium">
+                {TotalAmount}
+              </span>
+            </div>
           </div>
-          <div id="divPrecio" className="column text-right">
-            <span id="spanPrecio" className="text-lg text-medium">
-              {TotalAmount}
-            </span>
-          </div>
-        </div>
-        <div id="BtnHeader" className="toolbar-dropdown-group">
-          <div id="divFooter1" className="column">
-            <Link to="/Cart">
+          <div id="BtnHeader" className="toolbar-dropdown-group">
+            <div id="divFooter1" className="column">
+              <Link to="/Cart">
+                <a
+                  id="BtnHeaderCart"
+                  className="btn btn-sm btn-block btn-secondary"
+                  href="cart.html"
+                >
+                  Mi Carrito
+                </a>
+              </Link>
+            </div>
+            <div id="divFooter2" className="column">
               <a
-                id="BtnHeaderCart"
-                className="btn btn-sm btn-block btn-secondary"
-                href="cart.html"
+                id="BtnHeaderCart2"
+                className="btn btn-sm btn-block btn-success"
+                href="checkout-address.html"
               >
-                Mi Carrito
+                Comprar
               </a>
-            </Link>
-          </div>
-          <div id="divFooter2" className="column">
-            <a
-              id="BtnHeaderCart2"
-              className="btn btn-sm btn-block btn-success"
-              href="checkout-address.html"
-            >
-              Comprar
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
